afc14d7ed51660aee3147477918ac0afc51797a9
harish <kammalaharish3304@gmail.com>
2023-12-21 23:29:38 +0530

    6th

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,12 +1,65 @@
 import '../CssStykes/Profile.css';
 import Res from './Res';
 export default function Resume(){
-    return(
-       <>
-
-            <Res/>
-
-       
-       </>
-    )
+    return (
+        <div className="resume-container">
+          <h1>Harish Kammala</h1>
+          <p>Email: <a href="mailto:kammalaharish@gmail.com">kammalaharish@gmail.com</a></p>
+          <p>Phone: +91 9347779216</p>
+          <p>LinkedIn: <a href="https://www.linkedin.com/in/harish-kammala-425a811b0/" target="_blank" rel="noopener noreferrer">LinkedIn Profile</a></p>
+    
+          <h2>Education</h2>
+          <ul>
+            <li>Degree: B.Tech</li>
+            <li>College: Madanapalle Institute of Technology and Science</li>
+            <li>Passed: 2019-2023</li>
+            <li>Percentage: 83.7</li>
+          </ul>
+          <ul>
+            <li>Degree: Intermediate</li>
+            <li>College: Sri Chaitanya J.R College, Tirupathi</li>
+            <li>Passed: 2017-2019</li>
+            <li>Percentage: 98.8</li>
+          </ul>
+          <ul>
+            <li>Degree: SSC</li>
+            <li>School: Masthan Vignan EM High School</li>
+            <li>Passed: 2016-2017</li>
+            <li>Percentage: 97.0</li>
+          </ul>
+    
+          <h2>Skills</h2>
+          <ul>
+            <li>Programming Languages: Java (OOPs, Exception Handling, Core Java, Collection Framework, Spring Boot), Python (OOPs, Programming)</li>
+            <li>Web Technologies: HTML, CSS, React</li>
+            <li>Databases: MySQL</li>
+          </ul>
+    
+          <h2>Area Of Interest</h2>
+          <ul>
+            <li>Backend Development</li>
+            <li>Programming</li>
+            <li>Full Stack Development</li>
+          </ul>
+    
+          <h2>Projects</h2>
+          <h3>Bus Trading System</h3>
+          <ul>
+            <li>Tools & Technologies: HTML, CSS, React, Spring Boot, MySQL</li>
+            <li>My Role: Backend Developer</li>
+          </ul>
+    
+          <h3>Digital Library</h3>
+          <ul>
+            <li>Tools & Technologies: HTML, CSS, React, Spring Boot, MySQL</li>
+            <li>My Role: Backend Developer</li>
+          </ul>
+    
+          <h3>Quiz Application</h3>
+          <ul>
+            <li>Tools & Technologies: HTML, CSS, React, Spring Boot, MySQL</li>
+            <li>My Role: Backend Developer</li>
+          </ul>
+        </div>
+      );
 }
