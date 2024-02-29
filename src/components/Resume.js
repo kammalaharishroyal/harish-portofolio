b036b3044949862fbe8ece4726f8f022521c4efc
harish <kammalaharish3304@gmail.com>
2024-02-29 22:26:00 +0530

    resume update 1

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -7,7 +7,20 @@ export default function Resume(){
           <p>Email: <a href="mailto:kammalaharish@gmail.com">kammalaharish23@gmail.com</a></p>
           <p>Phone: +91 9347779216</p>
           <p>LinkedIn: <a href="https://www.linkedin.com/in/harish-kammala-425a811b0/" target="_blank" rel="noopener noreferrer">LinkedIn Profile</a></p>
-    
+          <table>
+            <tr>
+              <th>Degree</th>
+              <th>College</th>
+              <th>year</th>
+              <th>Percentage</th>
+              </tr>
+              <tr>
+              <td>B.Tech</td>
+              <td>Madanapalle Institute of Technology and Science</td>
+              <td>2019-23</td>
+              <td>83.7</td>
+            </tr>
+          </table>
           <h2>Education</h2>
           <ul>
             <li>Degree: B.Tech</li>
